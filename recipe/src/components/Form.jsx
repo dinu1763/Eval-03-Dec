06bd43e6ca7aa9ec45275180d4b847042bbcc00f
Dinesh <dinu1763@gmail.com>
2022-01-03 17:33:32 +0530

    display

--- recipe/src/components/Form.jsx
+++ recipe/src/components/Form.jsx
@@ -1,8 +1,38 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const Form = () =>{
-    const [form, setForm] = useState(null);
+    const [data, setData] = useState([]);
+    const [form, setForm] = useState("");
     const ref = useRef(null)
+
+    useEffect(() => {
+        getForm()
+    },[]);
+
+    const getForm = () =>{
+        fetch("http://localhost:9000/recipe").then(d => d.json()).then(res =>{
+            setData(res)
+        });
+    }
+
+    const addRecipe = () => {
+            const payload = {
+               title: form.title,
+               ingredients:form.ingredients,
+               ttc:form.ttc,
+               instructions:form.instruction,
+               image:ref.current,
+               status:false,
+           };
+           fetch("http://localhost:9000/recipe",{
+               method:"POST",
+               body: JSON.stringify(payload),
+               headers:{
+                   "content-type": "application/json",
+               },
+           });
+       }
+   
     const handleChange = (e) => {
         const { name, value } = e.target;
 
@@ -12,19 +42,39 @@ export const Form = () =>{
         });
     };
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(form)
-    }
+  
     
     return(
-        <form onSubmit={handleSubmit}>
+        <div>
+            
             <input onChange = {handleChange} type="text" name="title" placeholder="Title of the Recipe" /><br />
             <input onChange = {handleChange} type="text" name="ingredients" placeholder="Ingredients" /><br />
             <input onChange = {handleChange} type="number" name="ttc" placeholder="Time to Cook" /><br />
             <input onChange = {handleChange} type="text" name="instruction" placeholder="Instructions" /><br />
-            <input ref={ref} onChange = {handleChange} type="file" name="image" /><br />
-            <input type="submit" value="Add Recipe"/>
-        </form>
-    )
-}
+            {/* <input ref={ref} onChange = {handleChange} type="file" name="image" /><br /> */}
+            <button onClick={addRecipe}>Add Recipe</button>
+            
+            {data.map((e) =>(
+                <div>
+            <table >
+            <tr>
+                <th>Recipe</th>
+                {/* <th>Ingredients</th> */}
+                <th>Time to Cook</th>
+                <br />
+            </tr>
+            <tr>
+                <td>{e.title}</td>
+                {/* <td>{e.ingredients}</td> */}
+                <td>{e.ttc} Minutes</td>
+                <br />
+                <br />
+            </tr>
+            
+            </table>
+            </div>
+            ))}
+        </div>
+        
+    );
+};
